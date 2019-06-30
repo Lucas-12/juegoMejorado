baccf15dd11e0096fe8b9f3606c0c3d5c280b5ea
Lucas <47679348+Lucas-12@users.noreply.github.com>
2019-06-30 07:31:13 -0300

    -
    -

--- codigo.js
+++ codigo.js
@@ -81,76 +81,76 @@ function eliminaBotones(){
 function siguientePregunta(){
     switch(pregunta){
         case 1: 
-            preguntar("¿Cual de los siguientes animales vive mas años?", "1. Vaca", "2. Elefante", "3. Gato", "4. Paloma");
+            resCorrecta(3);
+            preguntar("¿Cuál de los siguientes animales vive mas años?", "1. Vaca", "2. Elefante", "3. Gato", "4. Paloma");
+            arrRes[1].setAttribute('name', 'correcto');
+            arrRes[2].setAttribute('name', 'incorrecto');
+            siResMal();
+            siResBien();
+            break;
+        case 2:
+            resCorrecta(2);
+            preguntar("¿En qué año se produjo la batalla de San Lorenzo?", "1. 1811", "2. 1801", "3. 1815", "4. 1813");
+            arrRes[3].setAttribute('name', 'correcto');
+            arrRes[1].setAttribute('name', 'incorrecto');
+            siResMal();
+            siResBien();
+            break;
+        case 4:
+            resCorrecta(4);
+            preguntar("¿Cuál es la capital de China?", "1. Shanghai ", "2. Hong Kong ", "3. Pekin", "4. Macao");
+            arrRes[2].setAttribute('name', 'correcto');
+            arrRes[3].setAttribute('name', 'incorrecto');
+            siResMal();
+            siResBien();
+            break;
+        case 6:
+            resCorrecta(3);
+            preguntar("¿En qué año se estreno la primer pelicula de la saga de Harry Potter?", "1. 2000", "2. 2001", "3. 2003", "4. 20005");
+            arrRes[1].setAttribute('name', 'correcto');
+            arrRes[2].setAttribute('name', 'incorrecto');
+            siResMal();
+            siResBien();
+        break;
+        case 8:
+            preguntar("¿Quién es el actual presidente de Mexico ", "1. Andrez Obrador", "2. Enrique Peña Nieto", "3. Vicente Quesada", "4. Ivan Duque Marquez");
+            arrRes[0].setAttribute('name', 'correcto');
+            arrRes[1].setAttribute('name', 'incorrecto');
+            siResMal();
+            siResBien();
+            resCorrecta(2);
+        break;
+        case 10:
+            preguntar("¿Cual de estos paises NO pertenece al continente asiatico?", "1. Indonesia", "2. Arabia Saudita", "3. Turquia", "4. Marruecos");
+            arrRes[3].setAttribute('name', 'correcto');
+            arrRes[0].setAttribute('name', 'incorrecto');
+            siResMal();
+            siResBien();
+            resCorrecta(1);
+        break;
+        case 12:
+            preguntar("¿Cuál de los siguientes paises es mas grande", "1. Iran", "2. Colombia", "3. India", "4. Egipto");
+            arrRes[2].setAttribute('name', 'correcto');
+            arrRes[3].setAttribute('name', 'incorrecto');
+            siResMal();
+            siResBien();
+            resCorrecta(4);
+        break;
+        case 14:
+            preguntar("¿Cuál es el gentilicio de Marruecos?", "1. Marroco", "2. Marroqui", "3. Marroquense", "4. Marrueses");
             arrRes[1].setAttribute('name', 'correcto');
             arrRes[2].setAttribute('name', 'incorrecto');
             siResMal();
             siResBien();
             resCorrecta(3);
-            break;
-        case 2:
-            preguntar("Cuantos aloskdsk", "1. Vaca", "2. Elefante", "3. Gato", "4. Paloma");
-            arrRes[3].setAttribute('name', 'correcto');
-            arrRes[1].setAttribute('name', 'incorrecto');
-            siResMal();
-            siResBien();
-            resCorrecta(4);
-            break;
-        case 3:
-            preguntar("aaaa", "1. Vaca", "2. Elefante", "3. Gato", "4. Paloma");
-            arrRes[2].setAttribute('name', 'correcto');
-            arrRes[1].setAttribute('name', 'incorrecto');
-            siResMal();
-            siResBien();
-            resCorrecta(3);
-            break;
-        case 4:
-            preguntar("gggggggg", "1. Vaca", "2. Elefante", "3. Gato", "4. Paloma");
-            arrRes[2].setAttribute('name', 'correcto');
-            arrRes[1].setAttribute('name', 'incorrecto');
-            siResMal();
-            siResBien();
-            resCorrecta(3);
         break;
-        case 5:
-            preguntar("llllllll", "1. Vaca", "2. Elefante", "3. Gato", "4. Paloma");
+        case 16:
+            preguntar("¿En qué año comenzo la Segunda Guerra Mundial?", "1. 1935", "2. 1936", "3. 1939", "4. 1951");
             arrRes[2].setAttribute('name', 'correcto');
             arrRes[1].setAttribute('name', 'incorrecto');
             siResMal();
             siResBien();
-            resCorrecta(3);
-        break;
-        case 6:
-            preguntar("eeeeeee", "1. Vaca", "2. Elefante", "3. Gato", "4. Paloma");
-            arrRes[2].setAttribute('name', 'correcto');
-            arrRes[1].setAttribute('name', 'incorrecto');
-            siResMal();
-            siResBien();
-            resCorrecta(3);
-        break;
-        case 7:
-            preguntar("kkkkkkkk", "1. Vaca", "2. Elefante", "3. Gato", "4. Paloma");
-            arrRes[2].setAttribute('name', 'correcto');
-            arrRes[1].setAttribute('name', 'incorrecto');
-            siResMal();
-            siResBien();
-            resCorrecta(3);
-        break;
-        case 8:
-            preguntar("qAAAAqqqqqq", "1. Vaca", "2. Elefante", "3. Gato", "4. Paloma");
-            arrRes[2].setAttribute('name', 'correcto');
-            arrRes[1].setAttribute('name', 'incorrecto');
-            siResMal();
-            siResBien();
-            resCorrecta(3);
-        break;
-        case 9:
-            preguntar("aaaaaaaaaaaaaaa", "1. Vacaaaaaaaaaa", "2. Eleaaaaaaafante", "3. Gato", "4. Paloma");
-            arrRes[2].setAttribute('name', 'correcto');
-            arrRes[1].setAttribute('name', 'incorrecto');
-            siResMal();
-            siResBien();
-            resCorrecta(3);
+            resCorrecta(2);
             decimePuntosYEliminaBotones();
         break;
     }
